f2ff110c0b72627008490a6342a57ae12dc269f4
pfitzpaddy <gis.patrick@gmail.com>
2016-03-26 16:32:59 +0430

    health - updated apis

--- config/routes.js
+++ config/routes.js
@@ -69,11 +69,6 @@ module.exports.routes = {
   'POST /health/data/contacts': 'Health/ProjectDashboardController.getContactListCsv',
   'POST /health/data/financials': 'Health/ProjectDashboardController.getFinancialListCsv',
 
-  // 'GET /health/data/details': 'Health/ProjectDashboardController.getProjectDetailsCsv',
-  // 'GET /health/data/locations': 'Health/ProjectDashboardController.getProjectLocationsCsv',
-  // 'GET /health/data/beneficiaries': 'Health/ProjectDashboardController.getProjectBeneficiariesCsv',
-
-
   // Dews 
   'POST /dews/indicator': 'Dews/DewsController.getIndicator',
   'POST /dews/chart': 'Dews/DewsController.getChart',
