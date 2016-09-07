e0cfcf3729495f713c322ef501f0a270b7d6b9f0
pfitzpaddy <gis.patrick@gmail.com>
2016-09-07 15:46:43 +0430

    HEALTH - new API to identify beneficiaries with incorrect locations

--- api/controllers/Health/ProjectController.js
+++ api/controllers/Health/ProjectController.js
@@ -231,28 +231,31 @@ module.exports = {
                 // return error
                 if ( err ) return res.negotiate( err );
 
+                console.log( l.admin1pcode )
+                console.log( b.admin1pcode )
+
                 // check if equal
-                if ( l[0].admin1pcode !== b.admin1pcode ) {
+                // if ( l.admin1pcode !== b.admin1pcode ) {
 
-                  // 
-                  console.log( l )
+                //   // 
+                //   console.log( l )
 
-                  var b = {
-                    id: b.id,
-                    admin1pcode: l.admin1pcode, 
-                    admin1name: l.admin1name, 
-                    admin2pcode: l.admin2pcode, 
-                    admin2name: l.admin2name, 
-                    admin1lng: l.admin1lng, 
-                    admin1lat: l.admin1lat,
-                    admin2lng: l.admin2lng, 
-                    admin2lat: l.admin2lat
-                  }                 
+                //   var b = {
+                //     id: b.id,
+                //     admin1pcode: l.admin1pcode, 
+                //     admin1name: l.admin1name, 
+                //     admin2pcode: l.admin2pcode, 
+                //     admin2name: l.admin2name, 
+                //     admin1lng: l.admin1lng, 
+                //     admin1lat: l.admin1lat,
+                //     admin2lng: l.admin2lng, 
+                //     admin2lat: l.admin2lat
+                //   }                 
 
-                  //
-                  ids.push( b );
+                //   //
+                //   ids.push( b );
 
-                }
+                // }
 
                 // return
                 counter++;
