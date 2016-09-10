30fc88162948a368f8f4a66829e3aff276be58bb
pfitzpaddy <gis.patrick@gmail.com>
2016-09-10 15:58:18 +0430

    HEALTH - db tasks update (backup)

--- tasks/db/ngm_reporthub_backup.sh
+++ tasks/db/ngm_reporthub_backup.sh
@@ -16,10 +16,12 @@ sudo mkdir $DIR
 # dump
 sudo mongodump --out $DIR
 # zip
-sudo zip -r $DIR/ngmReportHub.zip $DIR/ngmReportHub
-sudo zip -r $DIR/ngmHealthCluster.zip $DIR/ngmHealthCluster
-# copy to s3
-aws s3 cp $DIR/ngmReportHub.zip s3://s3-af-geonode/ngmReportHub/$STAMP
-aws s3 cp $DIR/ngmHealthCluster.zip s3://s3-af-geonode/ngmHealthCluster/$STAMP
+# sudo zip -r $DIR/ngmReportHub.zip $DIR/ngmReportHub
+# sudo zip -r $DIR/ngmHealthCluster.zip $DIR/ngmHealthCluster
+sudo zip -r $STAMP.zip $DIR
+# # copy to s3
+# aws s3 cp $DIR/ngmReportHub.zip s3://s3-af-geonode/ngmReportHub/$STAMP
+# aws s3 cp $DIR/ngmHealthCluster.zip s3://s3-af-geonode/ngmHealthCluster/$STAMP
+aws s3 cp $DIR/$STAMP.zip s3://s3-af-geonode/ngmHealthCluster/$STAMP
 # delete local
-# sudo rm -r -f $DIR
+sudo rm -r -f $DIR
