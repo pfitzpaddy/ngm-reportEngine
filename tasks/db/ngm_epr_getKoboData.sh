eece643dd3062af8ad76a657f907f592f879fe99
pfitzpaddy <gis.patrick@gmail.com>
2016-11-03 14:44:40 +0430

    EPR - added db task for kobo

---
+++ tasks/db/ngm_epr_getKoboData.sh
@@ -0,0 +1,7 @@
+﻿#!/bin/bash
+##################################################
+# ReportHub open reports email
+# Steps
+#		- run API
+##################################################
+curl http://dev.reporthub.immap.org/api/epr/getKoboData
