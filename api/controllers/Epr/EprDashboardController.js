03c6f1d66a4548cd40145a3132036560d44397d6
pfitzpaddy <gis.patrick@gmail.com>
2017-01-22 10:05:54 +0430

    EPR: Disasters updated to Incidents

--- api/controllers/Epr/EprDashboardController.js
+++ api/controllers/Epr/EprDashboardController.js
@@ -392,7 +392,7 @@ var EprDashboard = {
                                 '<div style="text-align:center"> with <span class="count">' + d.casualties + '</span> casualties from ' + d.disaster_type.replace(/_/g, ' ').toUpperCase() + ' disaster' +
                               '</div>' +  
                               '<div>' +
-                                '<div style="text-align:center">Disaster Category: ' + d['disaster_' + d.disaster_type + '_categories'].replace(/_/g, ' ').toUpperCase() +
+                                '<div style="text-align:center">Incident Category: ' + d['disaster_' + d.disaster_type + '_categories'].replace(/_/g, ' ').toUpperCase() +
                               '</div>' + 
                               '<div>' +
                                 'in ' + d.disaster_province_name + ', ' + d.disaster_district_name +
