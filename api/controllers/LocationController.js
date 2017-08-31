854c67c9ecf0591c579e43f85fa749fd9e6a7a6e
pfitzpaddy <pfitzgerald@immap.org>
2017-08-31 18:41:51 +0430

    Cluster: Updated ET target locations

--- api/controllers/LocationController.js
+++ api/controllers/LocationController.js
@@ -126,17 +126,18 @@ module.exports = {
   getAdmin3Facilities: function( req, res ) {
 
     // !admin0pcode || !admin1pcode
-    if ( !req.param( 'admin0pcode' ) || !req.param( 'admin1pcode' ) || !req.param( 'admin2name' ) || !req.param( 'admin3pcode' ) ) {
-       return res.json( 401, { msg: 'admin0pcode, admin1pcode, admin2name & admin3pcode required and must be string' });
+    if ( !req.param( 'cluster_id' ) || !req.param( 'admin0pcode' ) || !req.param( 'admin1pcode' ) || !req.param( 'admin2pcode' ) || !req.param( 'admin3pcode' ) ) {
+       return res.json( 401, { msg: 'cluster_id, admin0pcode, admin1pcode, & admin3pcode required and must be string' });
     }
 
     // get list
-    Admin2Facilities
+    Admin3Facilities
       .find()
       .where({ admin0pcode: req.param( 'admin0pcode' ) })
       .where({ admin1pcode: req.param( 'admin1pcode' ) })
-      .where({ admin2name: req.param( 'admin2name' ) })
+      .where({ admin2pcode: req.param( 'admin2pcode' ) })
       .where({ admin3pcode: req.param( 'admin3pcode' ) })
+      .where({ facility_class: req.param( 'cluster_id' ) })
       .sort('admin3name ASC')
       .exec( function( err, admin3 ){
 
