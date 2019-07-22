e36c2fdc02fadf20d0781eed922536f6e6def4c7
TimK <thedrfaustus@gmail.com>
2019-07-22 16:09:57 +0430

    feat(report): set and notify due dates by country

---
+++ config/notifications.js
@@ -0,0 +1,30 @@
+// config/notifications.js
+const REPORTING_DUE_DATE_NOTIFICATIONS_CONFIG = [
+  {
+    admin0pcode: "AF",
+    soon: [8],
+    pending: [12],
+    today: [10],
+    reporting_due_date: 10
+  }, {
+    admin0pcode: "CB",
+    soon: [8],
+    pending: [12],
+    today: [10],
+    reporting_due_date: 10
+  }, {
+    admin0pcode: "ET",
+    soon: [8],
+    pending: [12],
+    today: [10],
+    reporting_due_date: 10
+  }, {
+    admin0pcode: "ALL",
+    soon: [8],
+    pending: [12],
+    today: [10],
+    reporting_due_date: 10
+  }
+]
+
+module.exports.REPORTING_DUE_DATE_NOTIFICATIONS_CONFIG = REPORTING_DUE_DATE_NOTIFICATIONS_CONFIG;
