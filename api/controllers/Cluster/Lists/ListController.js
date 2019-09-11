672290df63341cec1209fbdf05a557d0d861bae7
fakhri <blitzfakhri@gmail.com>
2019-09-11 16:25:47 +0700

    fetch-activitieslist-based on role

--- api/controllers/Cluster/Lists/ListController.js
+++ api/controllers/Cluster/Lists/ListController.js
@@ -11,8 +11,33 @@ module.exports = {
   getActivities: function( req, res ) {
 
     // admin0pcode
-    var admin0pcode_filter = req.param( 'admin0pcode' ) ? { admin0pcode: { contains: req.param( 'admin0pcode' ) } } : {};
-
+		var admin0pcode_filter = req.param( 'admin0pcode' ) ? { admin0pcode: { contains: req.param( 'admin0pcode' ) } } : {};
+		
+		if (req.param('adminRpcode')){
+			var region_filter = req.param('adminRpcode').toLowerCase();
+			if(region_filter === 'hq'){
+				admin0pcode_filter = {}
+			}
+			if(region_filter === 'afro'){
+				admin0pcode_filter  = { or: [{ admin0pcode: { contains: 'CD' } }, { admin0pcode: { contains: 'ET' }},{ admin0pcode: { contains: 'NG' }},{ admin0pcode: { contains: 'SS' }}] }
+			}   
+			if(region_filter === 'amer'){
+				admin0pcode_filter = { admin0pcode: { contains: 'COL' } };
+			}
+			if(region_filter === 'emro'){
+				admin0pcode_filter = { or: [{ admin0pcode: { contains: 'AF' } }, { admin0pcode: { contains: 'SO' } }, { admin0pcode: { contains: 'SY' } }, { admin0pcode: { contains: 'YE' } }] }
+			}
+			if(region_filter === 'searo'){
+				admin0pcode_filter = {
+					admin0pcode: { contains: 'BD' },
+					admin0pcode: { contains: 'CB' },
+				};
+				admin0pcode_filter = { or: [{ admin0pcode: { contains: 'BD' } }, { admin0pcode: { contains: 'CB' } }] }
+			}
+			if(region_filter === 'euro'){
+				admin0pcode_filter = { admin0pcode: { contains: 'UA' } };
+			}
+		}
     // get activity list
     Activities
       .find()
