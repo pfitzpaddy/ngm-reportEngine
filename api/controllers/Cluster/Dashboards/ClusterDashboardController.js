ce72d75d7cdc2995b5e95b4799745c8b69ce614d
Tim <thedrfaustus@gmail.com>
2020-04-21 15:00:23 +0430

    update(download): add stock targeted groups

--- api/controllers/Cluster/Dashboards/ClusterDashboardController.js
+++ api/controllers/Cluster/Dashboards/ClusterDashboardController.js
@@ -1724,8 +1724,10 @@ var ClusterDashboardController = {
 								'site_lat',
 								'site_name',
 								'number_in_stock',
-								'number_in_pipeline',
+                'number_in_pipeline',
                 'beneficiaries_covered',
+                'stock_targeted_groups_id',
+                'stock_targeted_groups_name',
                 'remarks'
 							],
 
@@ -1768,8 +1770,10 @@ var ClusterDashboardController = {
 								'warehouse_lat',
 								'warehouse_name',
 								'number_in_stock',
-								'number_in_pipeline',
+                'number_in_pipeline',
                 'beneficiaries_covered',
+                'stock_targeted_groups_id',
+                'stock_targeted_groups_name',
                 'remarks'
               ];
 
